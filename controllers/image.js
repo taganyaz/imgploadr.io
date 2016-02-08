adf3a046f92566e35fd20601e435ffd45e888c3e
Edward <memwendwa@outlook.com>
2016-02-08 17:50:06 +0300

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,146 @@
+var fs = require('fs'),
+path = require('path'),
+sidebar = require('../helpers/sidebar'),
+Models = require('../models'),
+md5 = require('MD5');
+
+module.exports = {
+	index: function(req, res){
+		var viewModel={
+			image:{},
+			comments:[]
+		};
+		
+		Models.Image.findOne({filename:{ $regex: req.params.image_id}}, function(err,image){
+			if(err){throw err;}
+			
+			if(image){
+				image.views = image.views+1;
+				viewModel.image = image;
+				image.save();
+				
+				Models.Comment.find({image_id: image._id},{},{sort:{'timestamp': 1}}, function(err, comments){
+					if(err){throw err;}
+					
+					viewModel.comments = comments;
+					
+					sidebar(viewModel, function(viewModel){
+						res.render('image',viewModel);
+					});
+				});
+			}
+			else{
+				res.redirect('/');
+			}
+		});
+	},
+	create: function(req, res){
+		//res.send('The image:create POST controller');
+		var saveImage = function(){
+			var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
+			imgUrl = '';
+			for (var i = 0; i < 6; i += 1) {
+				imgUrl += possible.charAt(Math.floor(Math.random() *
+				possible.length));
+			}
+			//console.log(req.file);
+			//console.log("file name: " +req.file.name);
+			Models.Image.find({filename: imgUrl},function(err,images){
+				if(images.length >0){
+					saveImage();
+				}
+				else{
+					
+					var tempPath = req.file.path,
+					ext = path.extname(req.file.originalname).toLowerCase(),
+					targetPath = path.resolve('./public/upload/' + imgUrl + ext);
+					console.log("ext: " + ext);
+					if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext ===
+					'.gif') {
+						fs.rename(tempPath, targetPath, function (err) {
+							if (err) throw err;
+							//console.log("redirecting to /images/" + imgUrl);
+							var newImg = new Models.Image({
+								title: req.body.title,
+								filename: imgUrl + ext,
+								description: req.body.description
+							});
+							newImg.save(function(err,image){
+								console.log("redirecting to /images/" + image.uniqueId);
+								res.redirect('/images/' + image.uniqueId);
+							});
+						});
+					} else {
+						fs.unlink(tempPath, function (err) {
+							if (err) throw err;
+							res.json(500, { error: 'Only image files are allowed.' });
+						});
+					}
+				}
+			});
+		};
+		saveImage();
+	},
+	like: function(req, res){
+		//res.send('The image:like POST controller');
+		//res.json({likes:1});
+		Models.Image.findOne({filename: { $regex: req.params.image_id}}, function(err, image){
+			if(!err && image){
+				image.likes = image.likes + 1;
+				image.save(function(err){
+					if(err){
+						res.json(err);
+					}
+					else{
+						res.json({likes: image.likes});
+					}
+				});
+			}
+		});
+	},
+	comment: function(req, res){
+		//res.send('The image:comment POST controller');
+		Models.Image.findOne({filename: { $regex: req.params.image_id}},function(err,image){
+			if(!err && image){
+				var newComment = new Models.Comment(req.body);
+				newComment.gravatar = md5(newComment.email);
+				newComment.image_id = image._id;
+				
+				newComment.save(function(err, comment){
+					if(err){throw err;}
+					
+					res.redirect('/images/' + image.uniqueId + '#' + comment._id);
+				});
+			}
+			else
+			{
+				res.redirect('/');
+			}
+		});
+	},
+	remove: function(req, res){
+		Models.Image.findOne({filename: { $regex: req.params.image_id}},function(err, image){
+			if(err){throw err;}
+			
+			fs.unlink(path.resolve('./public/upload/' + image.filename),function(err){
+				if(err){
+					console.log(err);
+					throw err;
+					}
+				
+				Models.Comment.remove({image_id: image._id}, function(err){
+					if(err){throw err;}
+					
+					image.remove(function(err){
+						if(!err){
+							res.json(true);
+						}
+						else{
+							res.json(false);
+						}
+					});
+				});
+			});
+		});
+	}
+};
---
+++ tests/mocha.test.js
@@ -0,0 +1,7 @@
+describe('Mocha', function(){
+	describe('First Test', function(){
+		it('should assert 1 eqauls 1', function(){
+			expect(1).to.equal(1);
+		});
+	});
+});
